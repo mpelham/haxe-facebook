a402afdd3d11cf54db1d467f14acee28a9b1696c
Thomas Uster <thomasuster@gmail.com>
2015-07-08 20:19:09 -0700

    asd

--- project/iPhone/Facebook.mm
+++ project/iPhone/Facebook.mm
@@ -3,6 +3,10 @@
 namespace facebook {
 
     void _activateApp() {
+        printf("HELLO WILLEM\n");
+        printf("HELLO WILLEM\n");
+        printf("HELLO WILLEM\n");
+        printf("HELLO WILLEM\n");
         [FBSDKAppEvents activateApp];
     }
 }
